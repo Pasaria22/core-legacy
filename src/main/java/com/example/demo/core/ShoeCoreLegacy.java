859c616cb58e8a59ff94c2a9cca648d8c3e9c94c
Álvaro Soriano Vilches <alsovi90@gmail.com>
2022-10-16 13:21:23 +0200

    feat: endpoint patch done

--- src/main/java/com/example/demo/core/ShoeCoreLegacy.java
+++ src/main/java/com/example/demo/core/ShoeCoreLegacy.java
@@ -2,9 +2,10 @@ package com.example.demo.core;
 
 import com.example.demo.dto.in.ShoeFilter;
 import com.example.demo.dto.in.ShoeFilter.Color;
+import com.example.demo.dto.in.ShoesIn;
 import com.example.demo.dto.out.Shoe;
 import com.example.demo.dto.out.Shoes;
-import com.example.demo.dto.out.updated.Stock;
+import com.example.demo.dto.out.v2.Stock;
 
 import java.math.BigInteger;
 import java.util.List;
@@ -25,6 +26,11 @@ public class ShoeCoreLegacy extends AbstractShoeCore {
 
   @Override
   public Stock findAllStock() {
+    return null;
+  }
+
+  @Override
+  public ShoesIn patchStock(ShoesIn shoesIn) {
     return null;
   }
 }
